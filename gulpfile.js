b1ea182f9a330e85cae606b671d8845885c4d06f
Kolya Mudrenko <kolyamudrenko.v@gmail.com>
2023-10-08 23:24:44 +0300

    Kolya pz 1-8

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+gulp.task('scss', function () {
+  return gulp.src('./app/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./app/css'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch('./app/scss/**/*.scss', gulp.series('scss'));
+});
+
+gulp.task('default', gulp.series('scss', 'watch'));
