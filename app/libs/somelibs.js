cc992508dbcc0dd406cda610d2d0466b178d347a
= <=>
2023-10-08 11:07:30 +0300

    change somelibs.js

--- app/libs/somelibs.js
+++ app/libs/somelibs.js
@@ -1,3 +1,32 @@
-(function (){
-    console.log('some libs from folder libs, this is example for showing build process using gulp')
-})()
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.style.display = i === index ? 'block' : 'none';
+        slide.style.transition = 'opacity 0.5s';
+    });
+}
+
+function goToSlide(index) {
+    currentSlide = (index + slides.length) % slides.length;
+    showSlide(currentSlide);
+}
+
+function nextSlide() {
+    goToSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    goToSlide(currentSlide - 1);
+}
+
+prevBtn.addEventListener('click', prevSlide);
+nextBtn.addEventListener('click', nextSlide);
+
+showSlide(currentSlide);
+
+const slideInterval = 3000;
+setInterval(nextSlide, slideInterval);
