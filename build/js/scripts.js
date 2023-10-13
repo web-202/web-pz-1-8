a9636bd2ef273dec3bf8da18edee177549af6461
Dmytro <zaharkodima011@gmail.com>
2023-10-13 19:59:08 +0300

    zaharko_202

---
+++ build/js/scripts.js
@@ -0,0 +1,82 @@
+// Custom Scripts
+// Custom scripts
+// Мобильное меню бургер
+function burgerMenu() {
+    const burger = document.querySelector('.burger')
+    const menu = document.querySelector('.menu')
+    const body = document.querySelector('body')
+    burger.addEventListener('click', () => {
+      if (!menu.classList.contains('active')) {
+        menu.classList.add('active')
+        burger.classList.add('active-burger')
+        body.classList.add('locked')
+      } else {
+        menu.classList.remove('active')
+        burger.classList.remove('active-burger')
+        body.classList.remove('locked')
+      }
+    })
+    // Вот тут мы ставим брейкпоинт навбара
+    window.addEventListener('resize', () => {
+      if (window.innerWidth > 991.98) {
+        menu.classList.remove('active')
+        burger.classList.remove('active-burger')
+        body.classList.remove('locked')
+      }
+    })
+  }
+  burgerMenu()
+  
+  
+  // Вызываем эту функцию, если нам нужно зафиксировать меню при скролле.
+  function fixedNav() {
+    const nav = document.querySelector('nav')
+  
+    // тут указываем в пикселях, сколько нужно проскроллить что бы наше меню стало фиксированным
+    const breakpoint = 1
+    if (window.scrollY >= breakpoint) {
+      nav.classList.add('fixed__nav')
+    } else {
+      nav.classList.remove('fixed__nav')
+    }
+  }
+  window.addEventListener('scroll', fixedNav)
+
+  const slider = document.querySelector('.slider');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+
+let currentIndex = 0;
+
+function showSlide(index) {
+    const slides = slider.querySelectorAll('img');
+    slides.forEach((slide, i) => {
+        if (i === index) {
+            slide.style.display = 'block';
+        } else {
+            slide.style.display = 'none';
+        }
+    });
+}
+
+function prevSlide() {
+    currentIndex--;
+    if (currentIndex < 0) {
+        currentIndex = slider.querySelectorAll('img').length - 1;
+    }
+    showSlide(currentIndex);
+}
+
+function nextSlide() {
+    currentIndex++;
+    if (currentIndex >= slider.querySelectorAll('img').length) {
+        currentIndex = 0;
+    }
+    showSlide(currentIndex);
+}
+
+prevButton.addEventListener('click', prevSlide);
+nextButton.addEventListener('click', nextSlide);
+
+showSlide(currentIndex);
+
